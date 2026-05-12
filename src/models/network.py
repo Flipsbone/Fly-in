e3784f8554e75b7762506a608d4809a27277088f
Adrien Vacher <advacher@z1r12p2.42lyon.fr>
2026-05-12 10:54:16 +0200

    add : raise error same coordonate, same link

--- src/models/network.py
+++ src/models/network.py
@@ -1,10 +1,11 @@
-from src.parser.map_validator import Zone_Approval
+from src.parser.map_validator import Zone_Approval , Connection_Approval
 
 
 class Network:
     def __init__(self) -> None:
         self.nb_drones: int = 0
-        self.zones: dict = {}
-        self.connections: list = []
+        self.zones: dict[str, Zone_Approval]= {}
+        self.connections: dict[str, Connection_Approval] = {}
         self.start_node: Zone_Approval | None = None
         self.end_node: Zone_Approval | None = None
+        self.used_coordinates: set[tuple[int, int]] = set()
--- src/parser/map_parser.py
+++ src/parser/map_parser.py
@@ -1,4 +1,3 @@
-import sys
 from typing import TextIO
 from pydantic import ValidationError
 
@@ -12,19 +11,24 @@ from src.models.network import Network
 
 
 def parse_map(map_file: TextIO) -> Network:
-    find_drones_line = False
-    start_hub = 0
-    end_hub = 0
-    my_map = Network()
+    find_drones_line: bool = False
+    start_hub: int = 0
+    end_hub: int = 0
+    my_map: Network = Network()
+
     for i, line in enumerate(map_file):
         i += 1
-        clean_line = line.strip()
+        clean_line: str = line.strip()
+
         if clean_line == "" or clean_line.startswith("#"):
             continue
+
         if ":" not in clean_line:
             raise ValueError(f"--- line {i} --- \n"
                              f"Invalid format : {clean_line}")
-        extract_value = clean_line.split(":")
+
+        extract_value: list[str] = clean_line.split(":")
+
         if len(extract_value) > 2:
             raise ValueError(f"--- line {i} --- \n"
                              f"Invalid format in : {clean_line}")
@@ -34,11 +38,13 @@ def parse_map(map_file: TextIO) -> Network:
             case "nb_drones":
                 if find_drones_line:
                     raise ValueError(f"Line {i} Duplicate nb_drones")
+
                 if not find_drones_line:
                     find_drones_line = True
+
                 try:
-                    value = int(extract_value[1].strip())
-                    validate_drone = Drone_Approval(nb_drones=value)
+                    value: int = int(extract_value[1].strip())
+                    validate_drone: Drone_Approval = Drone_Approval(nb_drones=value)
                     my_map.nb_drones = validate_drone.nb_drones
                 except ValueError:
                     raise ValueError(f"--- line {i} --- \n"
@@ -49,6 +55,7 @@ def parse_map(map_file: TextIO) -> Network:
                 if not find_drones_line:
                     raise ValueError("nb_drones must be at "
                                      "the beginning of the file")
+
                 if extract_value[0].strip() == "start_hub":
                     start_hub += 1
                 if extract_value[0].strip() == "end_hub":
@@ -57,11 +64,14 @@ def parse_map(map_file: TextIO) -> Network:
                     raise ValueError(f"--- line {i} --- \n"
                                      f"There must be exactly one"
                                      f"'{extract_value[0]}' be write")
-                parts = extract_value[1].strip().split(maxsplit=3)
+
+                parts: list [str] = extract_value[1].strip().split(maxsplit=3)
+
                 if len(parts) < 3:
                     raise ValueError (f"--- line {i} --- \n"
                                       f"Invalid format line: {clean_line} must be : "
                                       "<name> <x> <y> [metadata](optional)")
+
                 try:
                     zone_data = {
                         "name": parts[0],
@@ -70,6 +80,11 @@ def parse_map(map_file: TextIO) -> Network:
                         "metadata": parts[3] if len(parts) > 3 else ""
                     }
                     validate_zone = Zone_Approval.model_validate(zone_data)
+                    coords: tuple[int, int] = (validate_zone.x , validate_zone.y)
+                    if coords in my_map.used_coordinates:
+                        raise ValueError(f"--- line {i} --- \n"
+                                         f"Parsing Error: Duplicate coordinates '{coords}'")
+                    my_map.used_coordinates.add(coords)
                     my_map.zones[validate_zone.name] = validate_zone
                     if extract_value[0].strip() == "start_hub":
                         my_map.start_node = validate_zone
@@ -83,21 +98,25 @@ def parse_map(map_file: TextIO) -> Network:
                 if not find_drones_line:
                     raise ValueError("nb_drones must be at "
                                      "the beginning of the file")
-                connections_data = extract_value[1].strip().split(maxsplit=2)
+
+                connections_data: list[str] = extract_value[1].strip().split(maxsplit=2)
+
                 if "-" not in connections_data[0]:
                     raise ValueError (f"--- line {i} --- \n"
                                       f"Invalid format line: {clean_line} must be : "
                                       "Connection1-Connection2 only ")
  
-                list_connections = connections_data[0].split("-")
-                nb_connections = len(list_connections)
+                list_connections: list[str] = connections_data[0].split("-")
+
+                nb_connections: int = len(list_connections)
+
                 if nb_connections > 2:
                     raise ValueError (f"--- line {i} --- \n"
                                       f"Invalid format line: {clean_line} must be : "
                                       "Connection1-Connection2 only ")
 
                 try:
-                    connections = {
+                    connections: dict [str, str] = {
                         "link_1": list_connections[0],
                         "link_2": list_connections[1],
                         "metadata": connections_data[1] if len(
@@ -106,10 +125,16 @@ def parse_map(map_file: TextIO) -> Network:
                     if not list_connections[0] in my_map.zones or not list_connections[1] in my_map.zones:
                         raise ValueError (f"--- line {i} --- \n"
                                           "Connections must link only previously defined zones using")
+
                     validate_connection = Connection_Approval.model_validate(
                         connections)
-                    #BEFORE ADDING, CHECK IF IT EXISTS IN THE ORDER LINK1 LINK2 OR LINK2 LINK1 IN MYMAP.CONNECTIONS.KEYS
-                    my_map.connections.append(validate_connection)
+
+                    unique_key: str = f"{validate_connection.link_1}-{validate_connection.link_2}"
+                    if unique_key in my_map.connections:
+                        raise ValueError(f"--- line {i} --- \n"
+                                         f"Parsing Error: Duplicate connection '{unique_key}'")
+
+                    my_map.connections[unique_key] = validate_connection
                 except ValidationError as e:
                     msg = e.errors()[0]['msg']
                     raise ValueError(f"--- line {i} --- \n Parsing Error : {msg}")
