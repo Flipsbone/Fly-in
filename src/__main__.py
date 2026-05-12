bbae6c2faf627a64c107ab39f84e36df0268009f
flipsbone <219644104+Flipsbone@users.noreply.github.com>
2026-05-12 21:55:03 +0200

    add : mypy flake8 dependancies

--- src/__main__.py
+++ src/__main__.py
@@ -10,6 +10,7 @@ def main() -> None:
     try:
         with open(args.map_path, "r") as map_file:
             my_network = parse_map(map_file)
+            print(my_network)
     except PermissionError as e:
         print(f"Permission Error: {e}.", file=sys.stderr)
         sys.exit(1)
